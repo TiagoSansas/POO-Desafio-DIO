e0dc64916f9302e0d30c1fbc985780a5e9e4e83c
Tiago Souza <tiagoeduardosouza@gmail.com>
2024-03-09 12:22:51 -0300

    Implementando funcionalidades Aparelho Telefonico

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import iphone.Iphone;
+
 public class App {
   public static void main(String[] args) {
+    Iphone iphone = new Iphone();
 
+    iphone.selecionarMusica();
+    iphone.tocar();
+    iphone.pausar();
+
+    iphone.atender();
+    iphone.ligar();
+    iphone.iniciarCorreioVoz();
   }
 }
--- src/iphone/Iphone.java
+++ src/iphone/Iphone.java
@@ -1,8 +1,9 @@
 package iphone;
 
+import aparelhoTelefonico.AparelhoTelefonico;
 import reprodutor.ReprodutorMusical;
 
-public class Iphone implements ReprodutorMusical {
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico {
 
   @Override
   public void tocar() {
@@ -18,5 +19,20 @@ public class Iphone implements ReprodutorMusical {
   @Override
   public void selecionarMusica() {
     System.out.println("SELECIONANDO MUSICA");
+  }
+
+  @Override
+  public void ligar() {
+    System.out.println("LIGANDO");
+  }
+
+  @Override
+  public void atender() {
+    System.out.println("ATENDER");
+  }
+
+  @Override
+  public void iniciarCorreioVoz() {
+    System.out.println("INCIANDO CORREIO DE VOZ");
   }
 }
